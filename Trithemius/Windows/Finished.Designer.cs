b5e135a9b6eca44e4805a027e78d3af2a86b1498
Tim Baxendale <pcluddite@outlook.com>
2020-01-18 15:03:29 -0600

    Fix 'Save As' button not moving on resize

--- Trithemius/Windows/Finished.Designer.cs
+++ Trithemius/Windows/Finished.Designer.cs
@@ -60,23 +60,21 @@ namespace Trithemius.Windows
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.textBox2.Location = new System.Drawing.Point(18, 18);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(780, 441);
+            this.textBox2.Size = new System.Drawing.Size(521, 288);
             this.textBox2.TabIndex = 38;
             // 
             // closeButton
             // 
             this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.closeButton.Location = new System.Drawing.Point(688, 471);
-            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.closeButton.Location = new System.Drawing.Point(459, 306);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(112, 35);
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
             this.closeButton.TabIndex = 39;
             this.closeButton.Text = "&Close";
             this.closeButton.UseVisualStyleBackColor = true;
@@ -84,9 +82,11 @@ namespace Trithemius.Windows
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(559, 471);
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(373, 306);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(122, 35);
+            this.saveButton.Size = new System.Drawing.Size(81, 23);
             this.saveButton.TabIndex = 40;
             this.saveButton.Text = "&Save As...";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -99,16 +99,15 @@ namespace Trithemius.Windows
             // Finished
             // 
             this.AcceptButton = this.closeButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.closeButton;
-            this.ClientSize = new System.Drawing.Size(818, 514);
+            this.ClientSize = new System.Drawing.Size(545, 334);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.textBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MinimumSize = new System.Drawing.Size(289, 201);
+            this.MinimumSize = new System.Drawing.Size(198, 144);
             this.Name = "Finished";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
